1ff073199624403dd7f61be960b9286b2d22b9b5
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-10-15 22:51:47 +0300

    Update project.tsx

--- components/project.tsx
+++ components/project.tsx
@@ -55,7 +55,6 @@ export default function Project({
 
         <Image
           src="/1.jpeg"
-          layout="responsive"
           width="192"
           height="192"
           alt="Project I worked on"
