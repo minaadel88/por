5bc18cbbd428b8d2cb95ca54cbfd989fc152fa66
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-11-05 07:53:32 +0200

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://www.linkedin.com/in/mina-adel-40162427"
+          href="https://www.linkedin.com/in/mina-adel-0b893b333"
           target="_blank"
         >
           <BsLinkedin />
