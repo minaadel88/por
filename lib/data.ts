53fc0b2e92576484eceeee309100866f18cfe6ec
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-11-22 18:17:46 +0200

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,71 +1,91 @@
-"use client";
-import { useRef } from "react";
-import { projectsData } from "@/lib/data";
-import Image from "next/image";
-import { motion, useScroll, useTransform } from "framer-motion";
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
 
-type ProjectProps = (typeof projectsData)[number];
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
 
-export default function Project({
-  title,
-  description,
-  tags,
-  imageUrl,
-  link,
-}: ProjectProps) {
-  const sectionRef = useRef<HTMLDivElement>(null);
+export const experiencesData = [
+  {
+    title: "Graduate from Ain Shams University",
+    location: "cairo, EG",
+    description:"Faculty of Commerce",
+    icon: React.createElement(LuGraduationCap),
+    date: "2013",
+  },
+  {
+    title: "Accountant At Armanious group",
+    location: "cairo, EG",
+    description:
+      "I worked in accounts receivable and general accounting related to budget preparation for the company, which was responsible for distributing all Eva Pharma products as well as beauty products.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2013 - 2015",
+  },
+  {
+    title: "Accountant at Egyptian Ministry of Interior",
+    location: "cairo, EG",
+    description:
+      "I am currently working as an accountant, where my role involves reviewing maintenance contracts and preparing financial reports for the payment of invoices related to those contracts. I also analyze data for the contracted companies to support timely decision-making.",
+    icon: React.createElement(FaReact),
+    date: "2015 - present",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Religious application",
+    description:
+"This is my first application, where I tried to implement what I learned, such as routing, navigation between pages, adding text features, formatting text, and including a video for each text section.",
+    tags: ["React", "Next.js",  "Tailwind"],
+    imageUrl: "/1.jpeg",
+    link:"https://first-project-psi-silk.vercel.app/",
+  },
+  {
+    title: "Kero Player",
+    description:
+      "A modern audio streaming app built with Vite.js, Firebase, and the YouTube API.",
+    tags: ["Vite.js", "Firebase", "YouTube API", "TypeScript"],
+    imageUrl: "/2.jpg", 
+    link: "https://kero-player.vercel.app",
+  },
   
-  const { scrollYProgress } = useScroll({
-    target: sectionRef,
-    offset: ["0 1", "1.33 1"],
-  });
-  
-  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+] as const;
 
-  return (
-    <motion.div
-      ref={sectionRef}
-      style={{
-        scale: scaleProgress,
-        opacity: opacityProgress,
-      }}
-      className="group mb-3 sm:mb-8 last:mb-0"
-    >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">
-            <a 
-              href={link} 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              {title}
-            </a>
-          </h3>
-          <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
-            {description}
-          </p>
-          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
-            {tags.map((tag, index) => (
-              <li
-                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
-                key={index}
-              >
-                {tag}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <Image
-          src={imageUrl}
-          width={192}
-          height={192}
-          alt="Project Image"
-          quality={95}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
-        />
-      </section>
-    </motion.div>
-  );
-}
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Redux",
+  "Framer Motion",
+] as const;
