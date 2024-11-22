daf2b07b2633efe4a580591ee312fd1a1d940ab2
minaadel88 <40057585+minaadel88@users.noreply.github.com>
2024-11-22 16:04:26 +0200

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -14,17 +14,19 @@ export default function Project({
   imageUrl,
   link,
 }: ProjectProps) {
-  const ref = useRef<HTMLDivElement>(null); // Use the ref for the div element
+  const divRef = useRef<HTMLDivElement>(null); // Ref for the div element
+  const anchorRef = useRef<HTMLAnchorElement>(null); // Ref for the anchor element
   const { scrollYProgress } = useScroll({
-    target: ref,
+    target: divRef,
     offset: ["0 1", "1.33 1"],
   });
+  
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
   return (
     <motion.div
-      ref={ref} // Ref assigned to motion.div, not the anchor <a> element
+      ref={divRef} // Ref assigned to motion.div
       style={{
         scale: scaleProgress,
         opacity: opacityProgress,
@@ -34,7 +36,12 @@ export default function Project({
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">
-            <a href={link} target="_blank" rel="noopener noreferrer">
+            <a 
+              href={link} 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              ref={anchorRef} // Ref assigned to anchor element
+            >
               {title}
             </a>
           </h3>
